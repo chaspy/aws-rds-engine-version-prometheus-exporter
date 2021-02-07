3d62bb9cf49adef9659a506991a891b49bcc4943
kondo takeshi <chaspy@users.noreply.github.com>
2021-02-07 16:52:33 +0900

    Add TestValidateEOLDate

---
+++ main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+type testdata struct {
+	name      string
+	validDate string
+	now       time.Time
+	out       string
+}
+
+func TestValidateEOLDate(t *testing.T) {
+	var layout = "2006-01-02"
+	nowForTest := "2021-01-01"
+	now, err := time.Parse(layout, nowForTest)
+	if err != nil {
+		t.Fatalf("failed to parse time: %#v", err)
+	}
+
+	tests := []testdata{
+		{name: "Expired", validDate: "2020-12-31", now: now, out: "expired"},
+		{name: "Alert", validDate: "2021-01-01", now: now, out: "alert"},
+		{name: "Alert", validDate: "2021-01-02", now: now, out: "alert"},
+		{name: "Warning", validDate: "2021-01-30", now: now, out: "alert"},
+		{name: "Warning", validDate: "2021-01-31", now: now, out: "warning"},
+		{name: "Warning", validDate: "2021-03-31", now: now, out: "warning"},
+		{name: "Warning", validDate: "2021-04-01", now: now, out: "ok"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := validateEOLDate(tt.validDate, tt.now)
+			if err != nil {
+				t.Fatalf("failed to calid EOL date: %#v", err)
+			}
+			if result != tt.out {
+				t.Fatalf("result wants %v but got %v when input validDate %v now %v", tt.out, result, tt.validDate, tt.now)
+			}
+		})
+	}
+}
